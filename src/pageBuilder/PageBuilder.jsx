b5c5cd3cca51e7764e9b1961a2495b2947be196b
Travis <tcarr8966@gmail.com>
2026-05-02 12:42:51 -0700

    update

--- src/pageBuilder/PageBuilder.jsx
+++ src/pageBuilder/PageBuilder.jsx
@@ -281,6 +281,11 @@ export default function PageBuilder() {
         <div className="pb-root">
             <aside className="pb-sidebar">
                 <div className="pb-topbar">
+                    <a href="#pages" className="pb-topbar-back" title="Back to pages">
+                        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                            <path d="M19 12H5M12 5l-7 7 7 7" />
+                        </svg>
+                    </a>
                     <div className="pb-topbar-logo">
                         <div className="pb-topbar-icon">
                             <GridIcon />
